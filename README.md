29d65a44183900ae7257906d2eaaa3111378c268
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-26 23:59:34 +0530

    show or hide modal functionality

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ IMPLEMENTATION:
 * Then we start building `Bookmark UI` which'll be served & inserted by javascript later on the submission of `modal-form`.
 * Next we start building our `modal` which pops up on the click on `ADD BOOKMARK` button.
 * Now a `modal` consists of two different parts - an `overlay` that darken everything behind it and the `modal` itself.
+* Also, the `modal` needs to be closed or shown according as the click on `close-icon` / `overlay` or on the `ADD BOOKMARK` button.
 
---
+++ script.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameElement = document.getElementById('website-name');
+const websiteUrlElement = document.getElementById('website-url');
+const bookmarkContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameElement.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (event) => (event.target === modal ? modal.classList.remove('show-modal') : false));
